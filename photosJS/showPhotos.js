625da2cf96c391cc119e12cda269ddfcfff94c55
Jovanveer Singh <randhawajovanveer@gmail.com>
2023-01-20 22:32:40 -0500

    first commit

---
+++ photosJS/showPhotos.js
@@ -0,0 +1,8 @@
+function showPhotos() {
+  var x = document.getElementById("portfolioPictures");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
